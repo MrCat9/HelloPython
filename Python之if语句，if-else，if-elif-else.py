8bde99c4341526d8a269ae6b9caf9ca95a3954ee
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-03-03 18:36:19 +0800

    Update and rename Python之if语句.py to Python之if语句，if-else，if-elif-else.py

--- Python之if语句，if-else，if-elif-else.py
+++ Python之if语句，if-else，if-elif-else.py
@@ -10,3 +10,24 @@ print('END')
 #注意: if 语句后接表达式，然后用:表示代码块开始。
 #如果 if 语句判断为 True，就会执行这个代码块。
 #缩进请严格按照Python的习惯写法：4个空格，不要使用Tab，更不要混合Tab和空格，否则很容易造成因为缩进引起的语法错误。
+
+age = 2
+#age = 20
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+#利用 if ... else ... 语句，我们可以根据条件表达式的值为 True 或者 False ，分别执行 if 代码块或者 else 代码块。
+#注意: else 后面有个“:”。
+
+age = 2
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+elif age >= 3:
+    print('kid')
+else:
+    print('baby')
+#baby
+#特别注意: 这一系列条件判断会从上到下依次判断，如果某个判断为 True，执行完对应的代码块，后面的条件判断就直接忽略，不再执行了。
