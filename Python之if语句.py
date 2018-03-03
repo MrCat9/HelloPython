d591a8fdd4b22689f1a39b793140412253e0ba05
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-03-03 17:19:07 +0800

    Create Python之if语句.py

---
+++ Python之if语句.py
@@ -0,0 +1,12 @@
+age = 20
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+print('END')
+#your age is 20
+#adult
+#END
+#注意: Python代码的缩进规则。具有相同缩进的代码被视为代码块，上面的3，4行 print 语句就构成一个代码块（但不包括第5行的print）。
+#注意: if 语句后接表达式，然后用:表示代码块开始。
+#如果 if 语句判断为 True，就会执行这个代码块。
+#缩进请严格按照Python的习惯写法：4个空格，不要使用Tab，更不要混合Tab和空格，否则很容易造成因为缩进引起的语法错误。
